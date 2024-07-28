f1092f69c6d0724e16e56ced11e0d812f8fc82d6
JennyPao18 <jenniferpaolapirirbor12@gmail.com>
2024-07-28 11:31:18 -0600

    first commit

---
+++ src/main/java/com/example/RailwayGitHub/RailwayGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.RailwayGitHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailwayGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailwayGitHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RailwayGitHub/RailwayGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RailwayGitHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailwayGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
